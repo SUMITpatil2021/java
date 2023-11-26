89685692b0405fb986c738dfdff09ac80b6022e1
SUMITpatil2021 <sumit.patil2021@gmail.com>
2023-11-26 21:24:07 +0530

    .

--- BasicsPrograms/src/fibo/Sumit.java
+++ BasicsPrograms/src/fibo/Sumit.java
@@ -4,7 +4,8 @@ public class Sumit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Sumit");
+System.out.println("Sumit"); 
+System.out.println("Shubham");
 	}
 
 }
